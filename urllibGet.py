56b8fdb3a89b90b0c5184a899f6d558fbce5ddd5
zhangtongqing <798998688@qq.com>
2018-05-28 18:20:04 +0800

    python 自动化测试实例

--- urllibGet.py
+++ urllibGet.py
@@ -2,6 +2,6 @@ import urllib.request
 import urllib.parse
 import json
 
-url = 'http://127.0.0.1:9999/api/luckdraw/app/myLuckRecord?token=92175173d26241f28044126a942656cb&userId=15371&sign=1234'
+url = 'http://211.159.153.228:8080/api/luckdraw/app/myLuckRecord?token=92175173d26241f28044126a942656cb&userId=15371&sign=1234'
 f = urllib.request.urlopen(url)
 print(f.read().decode('utf-8'));
