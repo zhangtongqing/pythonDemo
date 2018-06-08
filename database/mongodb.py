501023ec32815940282694050f5cd460c3930115
zhangtongqing <798998688@qq.com>
2018-06-08 14:22:13 +0800

    python操作mongodb示例

---
+++ database/mongodb.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+
+conn = MongoClient('211.159.153.228', 27017)
+db = conn.runoob    #连接runoob数据库，没有则自动创建
+my_set = db.test_set #使用test_set集合，没有则自动创建
+
+#添加多条数据到集合中
+def add():
+    print("...插入数据")
+    users=[{"name":"Andy.zhang","age":18},{"name":"mongodb","age":20}]
+    my_set.insert(users)
+
+def get():
+    print("......查询数据")
+    for i in my_set.find():
+        print(i)
+    #查询name=zhangsan的
+    for i in my_set.find({"name":"Andy.zhang"}):
+        print("条件查询：",i)
+    print("查询一个：",my_set.find_one({"name":"mongodb"}))
+
+if __name__ == '__main__':
+    get();
