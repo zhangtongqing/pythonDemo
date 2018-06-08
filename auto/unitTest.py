0e609ec9453dc959f29942ecd962738ab4f08b37
zhangtongqing <798998688@qq.com>
2018-06-08 11:55:04 +0800

    单元测试示例

--- auto/unitTest.py
+++ auto/unitTest.py
@@ -1,13 +1,22 @@
 import unittest
 
+import requests
+
 class TestMathFunc(unittest.TestCase):
 
-    def test_add(self):
+    def test_add1(self):
         print("单元测试第一个方法")
 
-    def test_add1(self):
-        print("单元测试第二个方法")
+    def test_add(self):
+        import json
+        # 请求URL
+        url = "http://211.159.153.228:8080/api/version/app/getMaxVersionByClient?t=ANDROID&v=2.0.1";
 
+        # **********************************************************************
+        ret = requests.get(url)
+        data = json.loads(ret.text)  # 转换成json对象
+        json = json.dumps(data, ensure_ascii=False, indent=2);  # 格式化json ensure_ascii=False 不编译中文，indent=2 换行
+        print(json)
 
 
 
